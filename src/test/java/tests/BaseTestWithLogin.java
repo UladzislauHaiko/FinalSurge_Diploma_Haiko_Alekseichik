3774e34221d67ee18a2629f76d6866266724430a
Uladzislau Haiko <tranceperfect@gmail.com>
2024-07-18 12:59:37 +0300

    added mock-up of project

---
+++ src/test/java/tests/BaseTestWithLogin.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+import pages.LoginPage;
+import utils.DriverFactory;
+import utils.InvokedListener;
+import utils.PropertyReader;
+import utils.TestListener;
+
+
+@Listeners({TestListener.class, InvokedListener.class})
+public abstract class BaseTestWithLogin {
+    protected WebDriver driver;
+    protected LoginPage loginPage;
+
+
+    @BeforeClass(alwaysRun = true)
+    @Parameters({"browserName"})
+    public void setUp(@Optional("chrome") String browserName, ITestContext testContext) throws Exception {
+        driver = DriverFactory.getDriver(browserName);
+        testContext.setAttribute("driver", driver);
+        this.loginPage = new LoginPage(driver);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        this.driver.quit();
+    }
+}
